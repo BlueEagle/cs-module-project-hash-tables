cf905361e3cb32f8be93a528f6fb694de92469df
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-09-30 16:28:33 -0600

    wip markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -6,8 +6,48 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 # Your code here
+# ig = ["?", "!", "\"", "'", ":", ";", ",", ".", "-", "+", "=", "/", "\\", "|", "[", "]", "{", "}", "(", ")", "*", "^", "&"]
+# for c in ig:
+#         words = words.replace(c, "")
+sp = ["\n", "\t", "\r"]
+for c in sp:
+    words = words.replace(c, " ")
+words = words.split(" ")
+
+# print(words)
+follower_dict = {}
+keys = []
+for i in range(0, len(words)):
+    w = words[i]
+    keys.append(w)
+
+    if len(w) > 0: # the current word is not an empty string
+        if w in follower_dict: # there is an entry for that word
+            if i+1 < len(words): # the next word exists
+                next_word = words[i+1]
+                if len(next_word) > 0: # the next word is not empty
+                    follower_dict[w].append(next_word)
+        else:
+            if i+1 < len(words): # the next word exists
+                next_word = words[i+1]
+                if len(next_word) > 0: # the next word is not empty
+                    follower_dict[w] = [next_word]
+    
+# print(follower_dict)
+
 
 
 # TODO: construct 5 random sentences
 # Your code here
 
+def is_valid_start_word(word):
+    return (word[0] == "\"" and word[1].isupper()) or word[0].isupper()
+
+def markov():
+    start_word = 'yo'
+    while not is_valid_start_word(start_word):
+        start_word = random.choice(keys)
+        # print(f"Trying word: {start_word}")
+
+    print(start_word)
+markov()
